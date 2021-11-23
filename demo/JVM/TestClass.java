fda53e0c51a2e37efd06e8ad382a377d3264a34b
Peng Li <jylipeng@tju.edu.cn>
2021-11-23 21:00:39 +0800

    update jvm test demo

---
+++ demo/JVM/TestClass.java
@@ -0,0 +1,8 @@
+package demo.JVM;
+
+public class TestClass {
+    private int m;
+    public int inc() {
+        return m + 1;
+    }
+}
---
+++ languages/Stream/StreamDemo.java
@@ -0,0 +1,7 @@
+package languages.Stream;
+
+class StreamDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+    }
+}
