470b4a8a82a45d032b64f3f731644bc050aaa233
Peng Li <jylipeng@tju.edu.cn>
2021-12-09 23:37:45 +0800

    finish the BlockQueue demo

--- demo/Concurrency/BlockingQueue/BlockingQueueTest.java
+++ demo/Concurrency/BlockingQueue/BlockingQueueTest.java
@@ -12,9 +12,10 @@ import java.util.concurrent.ArrayBlockingQueue;
 // 该程序是在一个目录及它的所有子目录下搜索所有文件，打印出包含指定关键字的行。
 public class BlockingQueueTest {
     private static final int FILE_QUEUE_SIZE = 10;
+    // 查找线程数量
     private static final int SEARCH_THREADS = 10;
     private static final File DUMMY = new File("");
-    // 阻塞队列 
+    // 阻塞队列 使用队列数据结构作为一种同步机制
     private static ArrayBlockingQueue<File> queue = new ArrayBlockingQueue<>(FILE_QUEUE_SIZE);
 
     public static void main(String[] args) {
@@ -24,22 +25,40 @@ public class BlockingQueueTest {
             System.out.print("Enter keyword (e.g. volatile):");
             String keyword = in.nextLine();
 
-        Runnable enumerator = () -> {
-            try {
-                enumerate(new);
-            } catch {
-
-            }
-        }
-
-
-
-
-
-
+            Runnable enumerator = () -> {
+                try {
+                    enumerate(new File(directory));
+                    // 虚拟目录，用来标志队列内容结束
+                    queue.put(DUMMY);
+                } catch(InterruptedException e) {}
+            };
+    
+            // 开始一个生产者线程，讲所有温江放进一个阻塞队列中
+            new Thread(enumerator).start();
+            for (int i = 0; i < SEARCH_THREADS; i++) {
+                Runnable searcher = () -> {
+                    try {
+                        boolean done = false;
+                        while(!done) {
+                            File file = queue.take();
+                            if (file == DUMMY) {
+                                queue.put(file);
+                                done = true;
+                            } else {
+                                search(file, keyword);
+                            }
+                        }
+                    } catch (IOException e) {
+                    e.printStackTrace();
+                    } catch (InterruptedException e) {
+                    }
+               };
+               new Thread(searcher).start();
+            };
         }
     }
 
+    // 递归该目录，将子目录的所有文件放入阻塞队列中
     public static void enumerate(File directory) throws InterruptedException {
         File[] files = directory.listFiles();
         for (File file : files) {
@@ -51,9 +70,17 @@ public class BlockingQueueTest {
         }
     }
 
-
     public static void search(File file, String keyword) throws IOException {
-
+        System.out.printf("%s文件搜索线程开始%n", file.getName());
+        try (Scanner in = new Scanner(file, "UTF-8")) {
+            int lineNumber = 0;
+            while (in.hasNextLine()) {
+                lineNumber++;
+                String line = in.nextLine();
+                if (line.contains(keyword)) {
+                    System.out.printf("%s:%d:%s%n", file.getPath(), lineNumber, line);
+                }
+            }
+        }
     }
-    
 }
