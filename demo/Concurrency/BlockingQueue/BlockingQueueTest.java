5eef56f3e0473e3a6cf89f11f5020ee3b4d55d8f
Peng Li <jylipeng@tju.edu.cn>
2021-12-09 23:09:06 +0800

    update BlockingQueue demo

--- demo/Concurrency/BlockingQueue/BlockingQueueTest.java
+++ demo/Concurrency/BlockingQueue/BlockingQueueTest.java
@@ -1,6 +1,8 @@
 package demo.Concurrency.BlockingQueue;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 import java.util.concurrent.ArrayBlockingQueue;
 
 // 用例来自于《java核心技术 卷1》 p670，练习如何使用阻塞队列来控制一组线程，
@@ -12,7 +14,46 @@ public class BlockingQueueTest {
     private static final int FILE_QUEUE_SIZE = 10;
     private static final int SEARCH_THREADS = 10;
     private static final File DUMMY = new File("");
+    // 阻塞队列 
     private static ArrayBlockingQueue<File> queue = new ArrayBlockingQueue<>(FILE_QUEUE_SIZE);
 
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print("Enter base directory (e.g. /opt/jdk1.8.0/src):");
+            String directory = in.nextLine();
+            System.out.print("Enter keyword (e.g. volatile):");
+            String keyword = in.nextLine();
+
+        Runnable enumerator = () -> {
+            try {
+                enumerate(new);
+            } catch {
+
+            }
+        }
+
+
+
+
+
+
+        }
+    }
+
+    public static void enumerate(File directory) throws InterruptedException {
+        File[] files = directory.listFiles();
+        for (File file : files) {
+            if (file.isDirectory()) {
+                enumerate(file);
+            } else {
+                queue.put(file);
+            }
+        }
+    }
+
+
+    public static void search(File file, String keyword) throws IOException {
+
+    }
     
 }
