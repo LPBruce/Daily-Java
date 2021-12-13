60d00a8506bab251e3d813e78147835e967b5284
Peng Li <jylipeng@tju.edu.cn>
2021-12-13 23:57:24 +0800

    update the Threadpool demo

--- demo/Concurrency/ThreadPool/MatchCounter.java
+++ demo/Concurrency/ThreadPool/MatchCounter.java
@@ -1,4 +1,4 @@
-package demo.Concurrency.Future;
+package demo.Concurrency.ThreadPool;
 
 import java.io.File;
 import java.io.IOException;
@@ -9,7 +9,6 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
-import java.util.concurrent.FutureTask;
 
 // 返回值位整数
 // callable类似于Runnable, 但是具有返回值
@@ -29,26 +28,25 @@ public class MatchCounter implements Callable<Integer> {
     // callable接口核心函数，是线程执行的核心流程，表示注意返回值位Integer
     public Integer call() throws IOException {
         System.out.printf("开启一个文件遍历线程：当前文件名为 %s%n", directory.getName());
-        int count = 0;
+        count = 0;
+
         try {
             File[] files = directory.listFiles();
             // Future用来保存异步线程计算结果，这里是个Future列表, 用来保存遍历结果
             List<Future<Integer>> results = new ArrayList<>();
+
             for (File file : files) {
                 // 如果该file是个文件夹，意味着需要重新开启一个线程进行分析计算。
                 // 因为需要返回值，所以使用使用callable和Future组合的形式
                 // 这里也意味着，只要是搜索一个文件，就是开启一个线程
                 if (file.isDirectory()) {
                     // 如果是个文件夹，则新建一个Matcher类实例进行遍历
-                    MatchCounter counter = new MatchCounter(file, keyword);
+                    MatchCounter counter = new MatchCounter(file, keyword, pool);
                     // 将此实例传入FutureTash包装器，讲Matcher实现的callable接口转换成Future和Runnable，
                     // 返回实现二者的接口的实例 Task
-                    FutureTask<Integer> task = new FutureTask<>(counter);
+                    Future<Integer> result = pool.submit(counter);
                     // 将Tash接口实例放入Future列表中，用于后期获得线程结果
-                    results.add(task);
-                    // 新建线程，传入task
-                    Thread t = new Thread(task);
-                    t.start();
+                    results.add(result);
                 } else {
                     // 判断改文件是否有关键字，如果有的话，则+1
                     if (search(file)) {
--- demo/Concurrency/ThreadPool/ThreadPoolTest.java
+++ demo/Concurrency/ThreadPool/ThreadPoolTest.java
@@ -3,7 +3,6 @@ package demo.Concurrency.ThreadPool;
 import java.io.File;
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
@@ -17,10 +16,13 @@ public class ThreadPoolTest {
             System.out.print("Enter keyword (e.g. volatile):");
             String keyword = in.nextLine();
 
-            // 
+            // 通过执行器的newCachedThreadPool新建线程池
+            // 返回对象为实现了ExecutorService接口的ThreadPoolExecutor对象
             ExecutorService pool = Executors.newCachedThreadPool();
 
+            // Callable接口包装器，该包装器将线程池传入
             MatchCounter counter = new MatchCounter(new File(directory), keyword, pool);
+            // 将Callable对象交给线程池
             Future<Integer> result = pool.submit(counter);
 
             try {
@@ -29,8 +31,10 @@ public class ThreadPoolTest {
                 e.printStackTrace();
             } catch (InterruptedException e) {}
 
+            // 关闭该线程池
             pool.shutdown();
 
+            // 返回该线程池最大的线程数
             int largestPoolSize = ((ThreadPoolExecutor) pool).getLargestPoolSize();
             System.out.println("最大的线程池数" + largestPoolSize);
         }
