1054f1f2589ac20d018acccc13a2d1e557fcdd0d
Peng Li <jylipeng@tju.edu.cn>
2022-08-21 22:56:18 +0800

    add java performance note

--- languages/Stream/StreamDemo.java
+++ languages/Stream/StreamDemo.java
@@ -1,7 +1,11 @@
 package languages.Stream;
 
+// Stream Java 流库的使用
 class StreamDemo {
     public static void main(String[] args) {
+        Stream<String> song = Stream
+
+
         System.out.println("Hello, World.");
     }
 }
